5113545b5993295d2d584137463810cbb053a2fd
Philipp Korn <korn.philipp@gmail.com>
2021-03-20 18:37:45 +0100

    add time picker

--- gui/src/app/static_tabs/myInterests.tsx
+++ gui/src/app/static_tabs/myInterests.tsx
@@ -1,60 +1,142 @@
 import {
-  Grid,
   Box,
-  Flex,
   Text,
-  Image,
-  SiteVariablesPrepared,
-  List,
+  Button,
 } from "@fluentui/react-northstar";
 import React from "react";
+import "react-image-picker/dist/index.css";
 //@ts-ignore
-import ImagePicker from 'react-image-picker';
-import 'react-image-picker/dist/index.css';
+import ImagePicker from "react-image-picker";
 
-const interests = [
-  'beer.jpeg', 'cars.jpeg', 'cooking.jpeg', 'drawing.jpeg',
-  'football.jpeg', 'knitting.jpeg', 'photography.jpeg',
-  'rc.jpeg', 'tvseries.jpeg', 'yoga.jpeg', 'goat.jpeg',
-  'astronaut.jpeg'];
+import "react-datepicker/dist/react-datepicker.css";
+//@ts-ignore
+import DatePicker from "react-datepicker";
 
-export function MyInterestsTab() {
-  return (
-    <Box
-      style={{
-        margin: "1.6rem 1.25rem",
-      }}
-    >
-      <Text size="larger" weight="bold" as="h1" align="center">
-        My Interests
-      </Text>
-      <Text
-        as="p"
-        align="center"
-        variables={({ colorScheme }: SiteVariablesPrepared) => {
-          console.log(colorScheme.default.foreground2);
-          return {
-            border: "1px solid red",
-            backgroundColor: "red",
-          };
-        }}
-        styles={{ opacity: ".65" }}
-      >
-        Tell us your interests so you can be matched with collegues.
-      </Text>
-        <div style={{
-          maxWidth: "500px",
-          width: "50%",
-          margin: "0 auto",
-        }}>
-                  <ImagePicker 
-          images={interests.map((img) => ({src: `/raw/${img}`, value: img.split('.')[0]}))}
-          multiple
-          // onPick={onPick}
-        />
-        </div>
-
-    </Box>
-  );
+interface IProps {}
+interface IState {
+  fromDate: Date;
+  toDate: Date;
 }
 
+const interests = [
+  "beer.jpeg",
+  "cars.jpeg",
+  "cooking.jpeg",
+  "drawing.jpeg",
+  "football.jpeg",
+  "knitting.jpeg",
+  "photography.jpeg",
+  "rc.jpeg",
+  "tvseries.jpeg",
+  "yoga.jpeg",
+  "goat.jpeg",
+  "astronaut.jpeg",
+];
+
+export class MyInterestsTab extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      fromDate: new Date(),
+      toDate: new Date(),
+    };
+  }
+  render() {
+    return (
+      <Box
+        style={{
+          margin: "1.6rem 1.25rem",
+        }}
+      >
+        <Text size="larger" weight="bold" as="h1" align="center">
+          My Interests
+        </Text>
+
+        <Text size="medium" as="h3" align="center" styles={{ opacity: ".65" }}>
+          Tell us your interests so you can be matched with collegues.
+        </Text>
+
+        <div
+          style={{
+            maxWidth: "500px",
+            width: "50%",
+            margin: "0 auto",
+          }}
+        >
+          <ImagePicker
+            images={interests.map((img) => ({
+              src: `/raw/${img}`,
+              value: img.split(".")[0],
+            }))}
+            multiple
+            // onPick={onPick}
+          />
+        </div>
+        <Text size="large" weight="bold" as="h2" align="center">
+          Time Preferences
+        </Text>
+        <div
+          style={{
+            maxWidth: "500px",
+            width: "50%",
+            margin: "0 auto",
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              padding: "27px",
+            }}
+          >
+            <DatePicker
+              selected={this.state.fromDate}
+              onChange={(date: Date) => this.setState({ fromDate: date })}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="hh:mm aa"
+            />
+          </div>
+
+          <Text
+            size="medium"
+            as="p"
+            align="center"
+            styles={{ opacity: ".65", paddingTop: "13px" }}
+          >
+            to
+          </Text>
+          <div
+            style={{
+              padding: "27px",
+            }}
+          >
+            <DatePicker
+              style={{
+                padding: "27px",
+              }}
+              selected={this.state.toDate}
+              onChange={(date: Date) => this.setState({ toDate: date })}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="hh:mm aa"
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            maxWidth: "500px",
+            margin: "0 auto",
+            display: "flex",
+            width: "110px",
+          }}
+        >
+          <Button content="Save" loader="Hack bandwidth" />
+        </div>
+      </Box>
+    );
+  }
+}
